1305634e0ed2fe47823b1e4dcbe9f15e41129836
Sergey Lezhnin <s.lezhnin@gmail.com>
2013-07-17 23:22:04 +0600

    plural to singular fixed, added html5boilerplate

--- grouporder/settings.py
+++ grouporder/settings.py
@@ -122,8 +122,11 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'html5boilerplate',
     'group_order',
 )
+
+GA_TRACKING_CODE = 'NONE'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
