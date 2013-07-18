e2cf83d577949b8e297a40ca2141872f8b536087
Sergey Lezhnin <s.lezhnin@gmail.com>
2013-07-18 12:06:24 +0600

    auth added

--- group_order/urls.py
+++ group_order/urls.py
@@ -3,7 +3,8 @@ __author__ = 'sirius'
 from django.conf.urls import patterns, url
 from group_order import views
 
-urlpatterns = patterns('', url(r'^$', views.PurchaseListView.as_view(), name='index'),
+urlpatterns = patterns('', url(r'^$', views.index, name='index'),
+                       url(r'^logout/$', views.logout, name='logout'),
                        url(r'^(?P<pk>\d+)/$', views.PurchaseView.as_view(), name='purchase'),
                        url(r'^order/(?P<pk>\d+)/$', views.OrderView.as_view(), name='order'),
                        url(r'^item/(?P<pk>\d+)/$', views.ItemView.as_view(), name='item'),
--- group_order/views.py
+++ group_order/views.py
@@ -1,24 +1,48 @@
 # Create your views here.
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from django.views import generic
+from django.contrib import admin, auth
+from django.contrib.auth import views
+from django.contrib.auth import REDIRECT_FIELD_NAME
 from models import Purchase, Order, Item, Transfer
+from django.utils.translation import ugettext_lazy as _
 
 
-class PurchaseListView(generic.ListView):
-    def get_queryset(self):
-        return Purchase.objects.all().order_by('-due')
+def index(request):
+    if not request.user.is_authenticated():
+        return views.login(request, template_name='group_order/login.html', extra_context={REDIRECT_FIELD_NAME: reverse('group_order:index')})
+    return render(request, 'group_order/index.html',
+                  {'purchase_list': Purchase.objects.all().order_by('-due')})
 
 
-class PurchaseView(generic.DetailView):
+def logout(request):
+    return views.logout(request, next_page=reverse('group_order:index'))
+
+
+class DetailViewWithContext(generic.DetailView):
+    def get_context_data(self, **kwargs):
+        context = super(DetailViewWithContext, self).get_context_data(**kwargs)
+        obj = context[self.model.__name__.lower()]
+        context['title'] = unicode(obj)
+        self.update_context(context, obj)
+        return context
+
+    def update_context(self, context, obj):
+        pass
+
+
+class PurchaseView(DetailViewWithContext):
     model = Purchase
 
 
-class OrderView(generic.DetailView):
+class OrderView(DetailViewWithContext):
     model = Order
 
 
-class ItemView(generic.DetailView):
+class ItemView(DetailViewWithContext):
     model = Item
 
 
-class TransferView(generic.DetailView):
+class TransferView(DetailViewWithContext):
     model = Transfer
